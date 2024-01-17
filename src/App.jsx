b0baab97e17db65357daa998c9f44d363cfba073
ogunlade adebayo peter <ogunladeadebayopeter@gmail.com>
2024-01-17 11:47:49 -0100

    new file

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import About from "./components/About"
 import Main from "./components/Main"
 import Footer from "./components/footer"
 import Buttonn from "./components/Button"
+import Tenary from "./components/Tenary"
+import Assignment from "./components/assignment"
+
 function App () {
   return(
     <>
-    <About/>
-    <Footer/>
-    <Main/>
-    <Buttonn style="text-orange-500" title={"Edit"}/>
-    <Buttonn style="text-yellow-500" title={"Delete"}/>
-    <Buttonn title={"Create"}/>
+    <Assignment />
+    {/* <Tenary /> */}
+    {/* <About/> */}
+    {/* <Footer/> */}
+    {/* <Main/> */}
+    {/* <Buttonn style="text-orange-500" title={"Edit"}/> */}
+    {/* <Buttonn style="text-yellow-500" title={"Delete"}/> */}
+    {/* <Buttonn title={"Create"}/> */}
 
    </> 
   )
